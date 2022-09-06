d3d3e06ca86251d73c65c29c9ed53116b6f72377
T-Hash06 <tomasfelipe.p@gmail.com>
2022-09-06 06:44:24 -0500

    [CREATE] Cheked Todos

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 import "../styles/TodoItem.css";
 
 import { toggleTodo } from "../stores/Todos";
+import { useState } from "react";
 
 import CheckButton from "./CheckButton";
 
@@ -10,14 +11,19 @@ interface props {
 }
 
 export default function TodoItem({ todo, id }: props) {
+	const [done, setDone] = useState(todo.done);
+
 	const handleClick = () => {
+		setDone(!done);
 		toggleTodo(id);
 	};
-
 	return (
 		<>
-			<li className="todo-container">
-				<p className="text">{todo.content}</p>
+			<li className={`todo-container ${todo.done ? "done" : ""}`}>
+				<p className="text">
+					{todo.content}
+					<span className="decorator" />
+				</p>
 				<CheckButton onClick={handleClick} isCheked={todo.done} />
 			</li>
 		</>
